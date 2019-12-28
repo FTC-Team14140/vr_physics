089502065f54e5fc403ff90207843870dffadd79
James_Kenney <jkenney2@charter.net>
2019-12-28 08:43:01 -0500

    Added cylinder and box to Util3D

--- Controller/src/util3d/Util3D.java
+++ Controller/src/util3d/Util3D.java
@@ -1,5 +1,9 @@
 package util3d;
 
+import javafx.scene.Group;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Mesh;
+import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
 import javafx.scene.shape.VertexFormat;
 
@@ -71,9 +75,213 @@ public class Util3D {
 
     }
 
-    public TriangleMesh getOpenParametricMesh(double sMin, double sMax, double tMin, double tMax,
-                                              int numFacetS, int numFacetT, Param3DEqn equations){
+    public static TriangleMesh getParametricMesh(double sMin, double sMax, double tMin, double tMax,
+                                                 int numFacetS, int numFacetT, Param3DEqn equations){
         return getParametricMesh(sMin, sMax, tMin, tMax, numFacetS, numFacetT, false, false, equations);
+    }
+
+    public static Group cylinder(float radius, float height, int circumferenceFacets, int heightFacets,
+                                 int radiusFacets, boolean endCaps, PhongMaterial... materials){
+        Group group = new Group();
+
+        TriangleMesh tubeMesh = getParametricMesh(0, 2.0 * Math.PI, -height / 2.0, height / 2.0, circumferenceFacets,
+                heightFacets, new Param3DEqn() {
+                    public float x(float s, float t) {
+                        return radius * (float) Math.cos(s);
+                    }
+
+                    public float y(float s, float t) {
+                        return -t;
+                    }
+
+                    public float z(float s, float t) {
+                        return radius * (float) Math.sin(s);
+                    }
+                });
+
+        MeshView tubeMeshView = new MeshView(tubeMesh);
+
+        if (materials != null && materials.length >0) tubeMeshView.setMaterial(materials[0]);
+
+        group.getChildren().add(tubeMeshView);
+
+        if (!endCaps) return group;
+
+        TriangleMesh capMesh1 = getParametricMesh(0, 2.0 * Math.PI, 0, radius, circumferenceFacets,
+                radiusFacets, new Param3DEqn() {
+                    public float x(float s, float t) {
+                        return t * (float)Math.cos(s);
+                    }
+
+                    public float y(float s, float t) {
+                        return -height/2.0f;
+                    }
+
+                    public float z(float s, float t) {
+                        return t * (float)Math.sin(s);
+                    }
+                });
+
+        TriangleMesh capMesh2 = getParametricMesh(2.0 * Math.PI, 0, 0, radius, circumferenceFacets,
+                radiusFacets, new Param3DEqn() {
+                    public float x(float s, float t) {
+                        return t * (float)Math.cos(s);
+                    }
+
+                    public float y(float s, float t) {
+                        return height/2.0f;
+                    }
+
+                    public float z(float s, float t) {
+                        return t * (float)Math.sin(s);
+                    }
+                });
+
+        MeshView capMeshView1 = new MeshView(capMesh1);
+        MeshView capMeshView2 = new MeshView(capMesh2);
+
+        if (materials != null && materials.length > 0){
+            int nMat = materials.length;
+            capMeshView1.setMaterial( nMat == 1? materials[0] : materials[1]);
+            capMeshView2.setMaterial( nMat == 1? materials[0] : nMat == 2? materials[1] : materials[2]);
+        }
+
+        group.getChildren().addAll(capMeshView1, capMeshView2);
+        return group;
+    }
+
+    public static Group box(float length, float height, float depth, int lengthFacets, int heightFacets,
+                      int depthFacets, PhongMaterial... materials){
+        Group group = new Group();
+        TriangleMesh frontMesh = getParametricMesh(-length / 2, length / 2, height / 2, -height / 2, lengthFacets, heightFacets,
+                new Param3DEqn() {
+                    @Override
+                    public float x(float s, float t) {
+                        return s;
+                    }
+
+                    @Override
+                    public float y(float s, float t) {
+                        return t;
+                    }
+
+                    @Override
+                    public float z(float s, float t) {
+                        return -depth/2;
+                    }
+                });
+
+        TriangleMesh backMesh = getParametricMesh(-length / 2, length / 2, -height / 2, height / 2, lengthFacets, heightFacets,
+                new Param3DEqn() {
+                    @Override
+                    public float x(float s, float t) {
+                        return s;
+                    }
+
+                    @Override
+                    public float y(float s, float t) {
+                        return t;
+                    }
+
+                    @Override
+                    public float z(float s, float t) {
+                        return depth/2;
+                    }
+                });
+
+        TriangleMesh leftMesh = getParametricMesh(-depth / 2, depth / 2, -height / 2, height / 2, depthFacets, heightFacets,
+                new Param3DEqn() {
+                    @Override
+                    public float x(float s, float t) {
+                        return -length/2;
+                    }
+
+                    @Override
+                    public float y(float s, float t) {
+                        return t;
+                    }
+
+                    @Override
+                    public float z(float s, float t) {
+                        return s;
+                    }
+                });
+
+        TriangleMesh rightMesh = getParametricMesh(-depth / 2, depth / 2, height / 2, -height / 2, depthFacets, heightFacets,
+                new Param3DEqn() {
+                    @Override
+                    public float x(float s, float t) {
+                        return length/2;
+                    }
+
+                    @Override
+                    public float y(float s, float t) {
+                        return t;
+                    }
+
+                    @Override
+                    public float z(float s, float t) {
+                        return s;
+                    }
+                });
+
+        TriangleMesh topMesh = getParametricMesh(-length / 2, length / 2, -depth / 2, depth / 2, lengthFacets, depthFacets,
+                new Param3DEqn() {
+                    @Override
+                    public float x(float s, float t) {
+                        return s;
+                    }
+
+                    @Override
+                    public float y(float s, float t) {
+                        return height/2;
+                    }
+
+                    @Override
+                    public float z(float s, float t) {
+                        return t;
+                    }
+                });
+
+        TriangleMesh bottomMesh = getParametricMesh(-length / 2, length / 2, depth / 2, -depth / 2, lengthFacets, depthFacets,
+                new Param3DEqn() {
+                    @Override
+                    public float x(float s, float t) {
+                        return s;
+                    }
+
+                    @Override
+                    public float y(float s, float t) {
+                        return -height/2;
+                    }
+
+                    @Override
+                    public float z(float s, float t) {
+                        return t;
+                    }
+                });
+
+        MeshView frontView = new MeshView(frontMesh);
+        MeshView backView = new MeshView(backMesh);
+        MeshView leftView = new MeshView(leftMesh);
+        MeshView rightView = new MeshView(rightMesh);
+        MeshView topView = new MeshView(topMesh);
+        MeshView bottomView = new MeshView(bottomMesh);
+
+        group.getChildren().addAll(frontView, backView, leftView, rightView, topView, bottomView);
+
+        if (materials == null || materials.length == 0) return group;
+
+        for (int i=0; i<materials.length; i++){
+            ((MeshView)group.getChildren().get(i)).setMaterial(materials[i]);
+        }
+
+        for (int i=materials.length; i<6; i++){
+            ((MeshView)group.getChildren().get(i)).setMaterial(materials[materials.length-1]);
+        }
+
+        return group;
+
     }
 
 }
